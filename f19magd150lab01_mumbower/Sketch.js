6b5c76579e7de37a3479d38523d690124c4724d7
MumbowerCJ23 <58005980+MumbowerCJ23@users.noreply.github.com>
2019-11-20 13:15:50 -0600

    Add files via upload

---
+++ f19magd150lab01_mumbower/Sketch.js
@@ -0,0 +1,117 @@
+function setup() {
+  createCanvas(500,500);
+}
+
+function draw() {
+    background(150);
+
+    fill(255);
+    strokeWeight(2);
+    ellipse(34, 36, 200, 200);
+
+    fill(200);
+    strokeWeight(3);
+    rect(30, 500, 240, 300, 9);
+
+    
+    strokeWeight(3);
+    rectMode(CENTER);
+    rect(450, 340, 240, 500, 7);
+
+    fill(150);
+    strokeWeight(2);
+    ellipse(4, 2, 200, 200);
+
+    fill(200);
+    strokeWeight(3);
+    rect(240, 500, 160, 200);
+
+    noFill();
+    strokeWeight(2);
+    rect(100, 400, 20, 30);
+
+    fill(255);
+    strokeWeight(0);
+    rect(30, 400, 20, 30);
+
+    fill(255);
+    strokeWeight(0);
+    rect(30, 465, 20, 30);
+
+    fill(255);
+    strokeWeight(0);
+    rect(100, 465, 20, 30);
+
+    fill(255);
+    strokeWeight(0);
+    rect(200, 465, 40, 50);
+
+
+    fill(255);
+    strokeWeight(0);
+    rect(280, 465, 40, 50);
+
+    noFill(380,150)
+    strokeWeight(2);
+    rect(380, 150, 30, 40);
+
+    strokeWeight(10);
+    point(100,400);
+
+    strokeWeight(2);
+    line(100, 400, 100, 415);
+
+    strokeWeight(2);
+    line(102, 402, 95, 410);
+
+    strokeWeight(2);
+    line(98, 398, 105, 410);
+
+    strokeWeight(15);
+    point(380,150);
+
+    strokeWeight(3);
+    line(380, 150, 380, 170);
+
+    strokeWeight(3);
+    line(383, 153, 375, 165);
+
+    strokeWeight(3);
+    line(381, 151, 385, 165);
+
+    fill(255);
+    strokeWeight(0);
+    rect(465, 150, 30, 40);
+
+    fill(255);
+    strokeWeight(0);
+    rect(380, 220, 30, 40);
+
+    fill(255);
+    strokeWeight(0);
+    rect(465, 220, 30, 40);
+
+    fill(255);
+    strokeWeight(0);
+    rect(465, 295, 30, 40);
+
+    fill(255);
+    strokeWeight(0);
+    rect(380, 295, 30, 40);
+
+    fill(255);
+    strokeWeight(0);
+    rect(465, 370, 30, 40);
+
+    fill(255);
+    strokeWeight(0);
+    rect(380, 370, 30, 40);
+
+    fill(255);
+    strokeWeight(0);
+    rect(465, 445, 30, 40);
+
+    fill(255);
+    strokeWeight(0);
+    rect(380, 445, 30, 40);
+}
